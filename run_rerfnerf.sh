78f09de92386e7e990e94365d486e05633cedaa2
baodinh <baodinhtfb@gmail.com>
2026-08-23 10:11:17 +0700

    Cleanup workspace

--- run_rerfnerf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Compatibility alias for the Ref-NeRF batch script.
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-exec "${SCRIPT_DIR}/run_refnerf.sh" "$@"
--- run_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-conda run -n fastgs_env python train.py -s ./datasets/Ref-NeRF/refnerf/toaster -m ./output/test_asg --iterations 500 --eval
